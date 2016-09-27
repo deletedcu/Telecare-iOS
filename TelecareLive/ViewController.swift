be80966b016735bcad59962aa64f785bd7cc89e2
CUREST TREBOW <curest.trebow@gmail.com>
2016-09-26 17:03:48 -0700

    Add code for registration and saving of the sid

--- TelecareLive/ViewController.swift
+++ TelecareLive/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import KeychainSwift
 
 class ViewController: UIViewController {
 
@@ -25,6 +28,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
     
     @IBAction func login(_ sender: AnyObject) {
+        let username = email.text
+        let pass = password.text
+        
+        // Refactor later
+        
+        print(username!)
+        print(pass!)
+        
+        let passcombination = username! + ":" + pass!
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Basic " + passcombination.toBase64(),
+            "Accept": "application/json"
+        ]
+        
+        
+        Alamofire.request("http://dev-telecarelive.pantheonsite.io/api/v1/auth", headers: headers)
+            .responseJSON{ response in
+//                debugPrint(response)
+                let json = JSON(data: response.data!)
+                
+                debugPrint(json)
+                
+                let alert = UIAlertView()
+                alert.title = "Alert"
+                alert.message = json["sid"].string
+                alert.addButton(withTitle: "Ok")
+                alert.show()
+                
+                let keychain = KeychainSwift()
+                keychain.set(username!, forKey: "username")
+                keychain.set(json["sid"].string!, forKey: "sid")
+                
+                print(keychain.get("sid"))
+                print(keychain.get("username"))
+            }
+        
+        
     }
     
     @IBAction func register(_ sender: AnyObject) {
@@ -34,5 +75,20 @@ class ViewController: UIViewController {
     @IBAction func forgotPassword(_ sender: AnyObject) {
     }
     
+}
+
+extension String
+{
+    func fromBase64() -> String
+    {
+        let data = NSData(base64Encoded: self, options: NSData.Base64DecodingOptions(rawValue: 0))
+        return String(data: data! as Data, encoding: String.Encoding.utf8)!
+    }
+    
+    func toBase64() -> String
+    {
+        let data = self.data(using: String.Encoding.utf8)
+        return data!.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
+    }
 }
 
